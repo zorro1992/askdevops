d37030485387c0a2f552a645477b4e9a10dd9ffa
Raghunandana S K <skraghunandan11@gmail.com>
2021-11-23 07:05:12 +0100

    Python code

---
+++ python/module/random/01.py
@@ -0,0 +1,13 @@
+import random
+#help(random)
+
+def getAnswer(user_input):
+    r = random.randint(1, 9)
+    if user_input == r:
+        return 'You have won this round, Play again and earn more'
+    else:
+        return 'Try again'
+
+user_input = 5
+fortune = getAnswer(user_input)
+print(fortune)
