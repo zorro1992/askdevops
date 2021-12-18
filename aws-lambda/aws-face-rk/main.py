7b4eb01c35b0401137bb8101abf283de920acae4
Raghunandana S K <skraghunandan11@gmail.com>
2021-12-18 11:57:28 +0100

    Lambda Code

---
+++ aws-lambda/aws-face-rk/main.py
@@ -0,0 +1,15 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+  
+    BUCKET = "face-rk-2021"
+    KEY = "bollywood_1.png"
+    
+    def detect_faces(bucket, key, region="eu-central-1"):
+        rekognition = boto3.client("rekognition", region)
+        response = rekognition.recognize_celebrities(Image={'S3Object':{'Bucket': bucket,'Name': key}})
+        for celebrity in response['CelebrityFaces']:
+    	    print ('Name: ' + celebrity['Name'])
+    	    
+    detect_faces(BUCKET,KEY)
