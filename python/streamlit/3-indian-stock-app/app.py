96bf89d7daa08e0db9e29c46c7d486d0bb521279
Raghunandana S K <skraghunandan11@gmail.com>
2021-12-21 16:09:16 +0100

    streamlit new bse stock data

---
+++ python/streamlit/3-indian-stock-app/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from bsedata.bse import BSE
+b = BSE()
+print(b)
+# Output:
+# Driver Class for Bombay Stock Exchange (BSE)
+
+# to execute "updateScripCodes" on instantiation
+b = BSE(update_codes = True)
+
+tg = b.topGainers()
+print(tg)
+st.title("Top 10 Losers")
+for x in range(len(tg)):
+    #print(tg[x]['securityID'])
+    st.text(tg[x]['securityID'])
