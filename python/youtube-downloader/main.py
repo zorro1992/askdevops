2f64edeaf6249e03618624f2c99b3f1361598217
Raghunandana S K <skraghunandan11@gmail.com>
2022-01-16 16:19:06 +0100

    New changes

--- python/youtube-downloader/main.py
+++ python/youtube-downloader/main.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 DOWNLOAD_FOLDER="~/Desktop/"
-video_url="https://youtu.be/V71MaqqTe3s"
+video_url="https://youtu.be/9dvsqwyHtBk"
 video_obj=YouTube(video_url)
 stream = video_obj.streams.get_highest_resolution()
 stream.download(DOWNLOAD_FOLDER)
