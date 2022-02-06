ec2a4fcedb6bdb646d06ae4b4b6ce3e6b7144867
raghu <skraghunandan11@gmail.com>
2022-02-06 09:34:33 +0100

    lambda code

--- aws-data-project/lambda/data-engineering/data_ingestion_local.py
+++ aws-data-project/lambda/data-engineering/data_ingestion_local.py
@@ -6,27 +6,9 @@ import time
 import random
 
 def lambda_handler(event, context):
-    #Create a clinet for kinesis, This uses boto3 which is imported above
-    client = boto3.client('kinesis', region_name='eu-central-1')
 
-    #Partition key for passing to Kinesis , this is required to keep a unique ID
-    partition_key = str(uuid.uuid4())
-    
-    # How many user information we need at a time.
     number_of_results = 500
     r = requests.get('https://randomuser.me/api/?exc=login&results=' + str(number_of_results))
     data = r.json()["results"]
     print(data)
     print(type(data))
-    for i in range(10):
-        random_user_index = int(random.uniform(0, (number_of_results - 1)))
-        random_user = data[random_user_index]
-        random_user = json.dumps(data[random_user_index])
-        print("Starting to process , loop {}".format(i))
-        client.put_record(
-                StreamName='streaming-data',
-                Data=random_user,
-                PartitionKey=partition_key)
-        print("Put record action is complete")
-        time.sleep(random.uniform(0, 1))
-        print("Sleeping now for sometime")
